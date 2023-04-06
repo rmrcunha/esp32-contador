3ca4d879debbb2925d48003ddbd3c87f9068f5e7
Renan Cunha <renancdev@gmail.com>
2023-04-06 18:23:54 -0300

    Mudando lógica do ledOn e ledOff

--- js/index.js
+++ js/index.js
@@ -1,7 +1,13 @@
 function ledOn(){
     document.getElementById("estadoL").innerHTML = "Led ligado"
+    console.log("estadoLed: 1")
+    fetch('/led/on')
+    .then(response => response.text())
+    .then(text => console.log(text))
+    .catch(error => console.error(error));
 }
 
 function ledOff(){
     document.getElementById("estadoL").innerHTML = "Led desligado"
+    console.log("estadoLed: 0")
 }
